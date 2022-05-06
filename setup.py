52119a2bc7932f93b27bbb426afb76cda4bb2729
fabio-cumbo <fabio.cumbo@gmail.com>
2022-05-05 21:54:49 -0400

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,32 @@
+import sys, setuptools
+
+if sys.version_info[0] < 3:
+    sys.stdout.write("xyz requires Python 3 or higher. Your Python your current Python version is {}.{}.{}"
+                     .format(sys.version_info[0], sys.version_info[1], sys.version_info[2]))
+
+setuptools.setup(name="xyz",
+                 version="0.1.0",
+                 author="Fabio Cumbo",
+                 author_email="fabio.cumbo@gmail.com",
+                 url="http://github.com/fabio-cumbo/xyz",
+                 license="LICENSE",
+                 packages=setuptools.find_packages(),
+                 entry_points={
+                     "console_scripts": ["xyz = xyz.xyz:main"]
+                 },
+                 description="A novel method for assessing and measuring homophily in networks",
+                 long_description=open("README.md").read(),
+                 long_description_content_type="text/markdown",
+                 install_requires=[
+                     "tqdm",
+                     "pandas",
+                     "networkx",
+                     "seaborn",
+                     "matplotlib"
+                 ],
+                 zip_safe=False,
+                 classifiers=[
+                    "Programming Language :: Python :: 3",
+                    "License :: OSI Approved :: MIT License",
+                    "Operating System :: OS Independent",
+                 ])
